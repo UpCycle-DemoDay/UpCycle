dc23055039bb09cd793d0c79cb1f2be39387639f
Aluno <viniciussoaresyeh@gmail.com>
2023-11-26 14:56:13 -0300

    Atualização

--- Login-UpCycle/js/login.js
+++ Login-UpCycle/js/login.js
@@ -9,3 +9,16 @@ document.getElementById('open-login').addEventListener('click', moveOverlay)
 
 document.getElementById('open-register-mobile').addEventListener('click', moveOverlay)
 document.getElementById('open-login-mobile').addEventListener('click', moveOverlay)
+
+const cadastroF = document.getElementById('cadastroF')
+const loginF = document.getAnimations('loginF')
+const cadastroU = document.getElementById('cadastroU')
+const loginU = document.getAnimations('loginU')
+
+
+cadastroF.addEventListener('click', function(){
+    window.location.href = '../cadastro-ferrovelho/cadastroF.html'
+});
+loginF.addEventListener('click', function() {
+    window.location.href = '../areaF/ferrovelho.hrml'
+});
