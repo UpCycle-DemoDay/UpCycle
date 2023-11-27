d9feb165c8e3deaefd63f5ec6895b921b6f25bab
Aluno <viniciussoaresyeh@gmail.com>
2023-11-27 10:42:49 -0300

    linkagem dos logins

--- Login-UpCycle/js/login.js
+++ Login-UpCycle/js/login.js
@@ -4,21 +4,12 @@ const loginContainer = document.getElementById('login-container')
 
 const moveOverlay = () => loginContainer.classList.toggle('move')
 
-document.getElementById('open-register').addEventListener('click', moveOverlay)
-document.getElementById('open-login').addEventListener('click', moveOverlay)
+document.getElementById('open-register').addEventListener('click', moveOverlay);
+document.getElementById('open-login').addEventListener('click', moveOverlay);
 
-document.getElementById('open-register-mobile').addEventListener('click', moveOverlay)
-document.getElementById('open-login-mobile').addEventListener('click', moveOverlay)
-
-const cadastroF = document.getElementById('cadastroF')
-const loginF = document.getAnimations('loginF')
-const cadastroU = document.getElementById('cadastroU')
-const loginU = document.getAnimations('loginU')
+document.getElementById('open-register-mobile').addEventListener('click', moveOverlay);
+document.getElementById('open-login-mobile').addEventListener('click', moveOverlay);
 
 
-cadastroF.addEventListener('click', function(){
-    window.location.href = '../cadastro-ferrovelho/cadastroF.html'
-});
-loginF.addEventListener('click', function() {
-    window.location.href = '../areaF/ferrovelho.hrml'
-});
+
+
