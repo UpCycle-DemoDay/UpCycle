9d4909ccfb57f51a6554a34489b0e70639188b96
Larissa Carvalho <142347156+LarissaCarvalho1@users.noreply.github.com>
2023-12-13 11:38:41 -0300

    Adição de formatação dos campos.

--- CadastroFerrovelho/js/cadastroferrovelho.js
+++ CadastroFerrovelho/js/cadastroferrovelho.js
@@ -30,3 +30,34 @@ function formatarTelefone(input){
 
     input.value = numeroTelefone;
 }
+
+function formatarNumero(input) {
+    let valorAtual = input.value.replace(/\D/g, '');
+
+    if (valorAtual.length > 5) {
+        valorAtual = valorAtual.substring(0, 5);
+    }
+    input.value = valorAtual;
+}
+
+function formatarCep(input) {
+    let valorAtual = input.value.replace(/\D/g, '');
+
+    if (valorAtual.length > 8) {
+        valorAtual = valorAtual.substring(0, 8);
+    }
+
+    const partes = [];
+    partes.push(valorAtual.substring(0, 5)); 
+    partes.push(valorAtual.substring(5, 8)); 
+
+    input.value = partes.filter(Boolean).join('-');
+}
+
+function formatarUF(input) {
+    let valorAtual = input.value.toUpperCase();
+
+    valorAtual = valorAtual.replace(/[^A-Z]/g, '');
+
+    input.value = valorAtual;
+}
--- CadastroUsuario/js/cadastro02.js
+++ CadastroUsuario/js/cadastro02.js
@@ -1,3 +1,33 @@
+function formatarNumero(input) {
+    let valorAtual = input.value.replace(/\D/g, '');
+
+    if (valorAtual.length > 5) {
+        valorAtual = valorAtual.substring(0, 5);
+    }
+    input.value = valorAtual;
+}
+
+function formatarCep(input) {
+    let valorAtual = input.value.replace(/\D/g, '');
+
+    if (valorAtual.length > 8) {
+        valorAtual = valorAtual.substring(0, 8);
+    }
+
+    const partes = [];
+    partes.push(valorAtual.substring(0, 5)); 
+    partes.push(valorAtual.substring(5, 8)); 
+
+    input.value = partes.filter(Boolean).join('-');
+}
+
+function formatarUF(input) {
+    let valorAtual = input.value.toUpperCase();
+
+    valorAtual = valorAtual.replace(/[^A-Z]/g, '');
+
+    input.value = valorAtual;
+}
 
 document.getElementById("form-endereco").addEventListener("submit", function (event) {
     event.preventDefault();
